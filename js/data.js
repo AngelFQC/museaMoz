fd5d213fdc33d4c3b3a14959c132a9ff5c795b3e
Gustavo Huarcaya <diavolo@qqta.org>
2013-10-06 03:09:37 -0500

    Adición de museos de Ica pertenecientes al Sistema de Museos del Estado

--- js/data.js
+++ js/data.js
@@ -291,8 +291,98 @@ var dataApp = [
 							}
 						]
 					},
+				]
+			}
 		]
-	  }
-	]
-  }
+	},
+	{ // Primer nivel: Regiones del país
+		nombre: 'Ica',
+		provincias: [
+			{ // Segundo nivel: Provincias
+				nombre: 'Ica',
+				distritos: [
+					{ // Tercer nivel: Distritos
+						nombre: 'Ica',
+						museos: [
+							{ // Cuarto nivel: Museos
+								nombre: 'Museo Regional de Ica “Adolfo Bermúdez Jenkins”',
+								coleccion: 'Arqueología, Histórico-artística y Etnográfica',
+								descripcion: [
+									'El museo presenta dos salas de exposición.',
+									'La sala de Arqueología, con bienes culturales Paracas, Nasca, Wari e Inca; y la de Bioantropología, que detalla costumbres funerarias, deformaciones craneanas, trepanaciones, cabezas trofeo y restos humanos que evicencian paleopatologías.',
+									'En la parte posterior del idificio se encuentra una reproducción de las Líneas de Nasca que pueden ser apreciadas desde una plataforma.'
+								],
+								direccion: 'Jr. Ayabaca cuadra 8 s/n - Urb. San Isidro',
+				coordenadas: [ { latitud: "-14.0719255", longitud: "-75.7338782" } ],
+								atencion: [
+									{ dias: 'Lunes a viernes', horario: '08:00 - 19:00' },
+									{ dias: 'Sábado y Domingo', horario: '08:30 - 18:30' },
+								],
+								contacto: {
+									email: [ 'ica@mcultura.gob.pe' ],
+									telefonos: [
+										{ numeroMarcar: '+5156234383', numeroMostrar: '(056) 234383' }
+									],
+									web: null
+								}
+							}
+						]
+					},
+				]
+			},
+			{ // Segundo nivel: Provincias
+				nombre: 'Pisco',
+				distritos: [
+					{ // Tercer nivel: Distritos
+						nombre: 'Humay',
+						museos: [
+							{ // Cuarto nivel: Museos
+								nombre: 'Sala de Exhibición del Sitio Arqueológico “Tambo Colorado”',
+								coleccion: 'Arqueológica',
+								descripcion: [
+									'Presenta la secuencia cronológica cultural del valle de Pisco, así como fragmentos de bienes culturales recuperados en las investigaciones realizadas en el complejo arqueológico y su entorno.',
+									'Incluye además réplicas, fotografías, mapas e infografías que contribuyen a la comprensión de la zona arqueológica Inca.'
+								],
+								direccion: 'Km. 39 vía Los Libertadores Wari',
+				coordenadas: [ { latitud: "-13.7051206", longitud: "-75.8294273" } ],
+								atencion: [
+									{ dias: 'Lunes a domingo', horario: '09:00 - 17:00' },
+								],
+								contacto: {
+									email: [ 'ica@mcultura.gob.pe' ],
+									telefonos: [
+										{ numeroMarcar: '+5156234383', numeroMostrar: '(056) 234383' }
+									],
+									web: null
+								}
+							}
+						]
+					},
+					{ // Tercer nivel: Distritos
+						nombre: 'Paracas',
+						museos: [
+							{ // Cuarto nivel: Museos
+								nombre: 'Museo de Sitio de Paracas “Julio C. Tello”',
+								coleccion: 'Arqueológica',
+								descripcion: [
+									'El museo fue dañado por el sismo de Pisco del 15 de agosto de 2007.',
+									'Por esta razón se está construyendo un nuevo museo, que estará concluido e implementado para la vista del público en el año 2013.'
+								],
+								direccion: 'Km. 27 Carretera Pisco-Puerto San Martín (Reserva Natural de Paracas)',
+				coordenadas: [ { latitud: "-13.868209", longitud: "-76.2731731" } ],
+								atencion: [],  // No hay datos acerca de días y hora de atención
+								contacto: {
+									email: [],
+									telefonos: [
+										{ numeroMarcar: '+5156234383', numeroMostrar: '(056) 234383' }
+									],
+									web: null
+								}
+							}
+						]
+					},
+				]
+			}
+		]
+	}
 ];
